36822d1722ad8ea723ec43354edbc8ea6c23951c
Carlos Marcel <ca_marcel@hotmail.com>
2020-10-07 03:00:36 -0300

    Add files via upload
    Rest

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+//iniciando o App
 const app = express();
+// Iniciando o DB
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+//Inserindo Models
+requireDir('./src/models');
+
+//const Product = mongoose.model('Product');
+
+app.use('/api', require('./src/models/routes'));
 
 app.listen(3001);
